4b5c98722c54a6d728378d2b5192c14b9bf149e2
Joshua-Enrico <tmrfack@gmail.com>
2022-06-27 18:42:32 -0500

    testing changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { ClientModule } from './client/client.module';
 import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [ClientModule, CoreModule],
+  imports: [ClientModule, CoreModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
