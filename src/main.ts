5646b8e3a159b07975a5bc02c933f7afab35d9f5
Joshua-Enrico <tmrfack@gmail.com>
2022-06-27 18:48:38 -0500

    testing fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, 'assets/'), {
+  app.useStaticAssets(join(__dirname,"..", 'assets'), {
     prefix: '/docs'
   });
   app.useGlobalPipes(new ValidationPipe());
