85b4e652e5a16ea8c524e66c7c44a94a90da4df4
Joshua-Enrico <tmrfack@gmail.com>
2022-06-26 22:07:26 -0500

    Testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
