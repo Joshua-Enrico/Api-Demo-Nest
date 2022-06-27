e1de1cb1349e7a1bc896001610f766b80c69748a
Joshua-Enrico <tmrfack@gmail.com>
2022-06-27 17:28:32 -0500

    testing fix to serverless

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '/static'), {
+    prefix: '/docs',
+  });
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder().setTitle('Demo Application')
   .setDescription("Demo API Application")
   .setVersion('v1')
   .build();
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
